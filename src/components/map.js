2afc50dad5768419e1e35ddf9c065a62fb3e88c1
forbesmiyasato <miya5288@pacificu.edu>
2020-07-13 20:43:51 -0700

    draw polygon, but need correct county paths

--- src/components/map.js
+++ src/components/map.js
@@ -1,14 +1,36 @@
 import React from "react";
-import { withGoogleMap, GoogleMap } from "react-google-maps";
+import { withGoogleMap, GoogleMap, Polygon } from "react-google-maps";
+
+const coords = [
+  { lat: 40, lng: -70 },
+  { lat: 50, lng: -70 },
+  { lat: 50, lng: -80 },
+  { lat: 40, lng: -80 }
+];
 
 const Map = (props) => {
   const GoogleMapExample = withGoogleMap((props) => (
     <GoogleMap
       defaultCenter={{ lat: 39.8097343, lng: -98.5556199 }}
       defaultZoom={4}
-    ></GoogleMap>
+    >
+      <Polygon
+        path={coords}
+        key={1}
+        options={{
+          fillColor: "#000",
+          fillOpacity: 0.4,
+          strokeColor: "#000",
+          strokeOpacity: 1,
+          strokeWeight: 1,
+        }}
+        onClick={() => {
+          console.log("ahmet");
+        }}
+      />
+    </GoogleMap>
   ));
-  
+
   return (
     <div class="container">
       <GoogleMapExample
