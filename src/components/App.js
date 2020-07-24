e80a38a1c5c2c1b0ab790ff9ca9f32d7c18603a7
ThomasMLawrence <tmlawrence54@gmail.com>
2020-07-24 13:23:27 -0700

    finished banner - not scallable yet

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Map from './map';
+import '../App.css'
 
 const App = () => {
   return (
+    <view>
+      <div id="headerContainer">
+        <h1 id="dashboardName">Coronavirus US County Tracker</h1>
+      </div>
       <Map></Map>
+    </view>
+           
   )
 }
 
