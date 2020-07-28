d9844dacfb4dfebb7ffad9105c722246e7f6c981
forbesmiyasato <miya5288@pacificu.edu>
2020-07-28 15:58:57 -0700

    Integrated David's code

--- client/src/components/RightSideBar.js
+++ client/src/components/RightSideBar.js
@@ -1,15 +1,54 @@
-import React from 'react'
+import React from "react";
+import "../RightSideBar.css";
 
 const RightSideBar = (props) => {
-    return  (
-    <div class="side">
-        Test 2
-        <p>
-            {props.data.new}
-        </p>
+    console.log(props.data);
+    return (
+        <div class="side">
+            <div class="headerInfo">Cases overview</div>
+            <div id="state">
+                State:
+                <item id="byState">
+                    <p>
+                    {props.data.state_name}
+                    </p>
+                </item>
+            </div>
+            <div id="county">
+                County:
+                <item id="byCounty">
+                    <p>
+                    {props.data.county_name}
+                    </p>
+                </item>
+            </div>
+            <section class="grid-container">
+                <div class="grid-item confirmed">
+                    Confirmed:
+                    <p>{props.data.confirmed}</p>
+                </div>
+                <div class="grid-item income">
+                    New Cases:
+                    <p>{props.data.new}</p>
+                </div>
+                <div class="grid-item recovered">
+                    New Deaths:
+                    <p>{props.data.new_death}</p>
+                </div>
+                <div class="grid-item deaths">
+                    Total Deaths:
+                    <p>{props.data.death}</p>
+                </div>
+                <div class="grid-item race">
+                    Fatality Rate:
+                    <p>{props.data.fatality_rate}</p>
+                </div>
+                <div class="grid-item average-age">
+                    Average age:
+                </div>
+            </section>
         </div>
-    )
-
-}
+    );
+};
 
 export default RightSideBar;
