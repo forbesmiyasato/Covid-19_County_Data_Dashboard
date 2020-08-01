d8b82cfdfc5ef1f6184ad575df4d1533c2cb22a7
ThomasMLawrence <tmlawrence54@gmail.com>
2020-07-31 17:12:58 -0700

    got the data for the top five all counties

--- client/src/components/topCounties.js
+++ client/src/components/topCounties.js
@@ -3,13 +3,16 @@ import "../topCounties.css"
 
 const findGreatest = (data, number) => {
     //searching for the highest states up to a certain number
+    //this function does alot since we only want to call it
+    //once on page load
     let greatestDeaths = new Array();
+    let greatestCases = new Array();
+    let greatestFatality = new Array();
     let current = null;
     for(var i = 0; i < data.length; ++i){
-        //if county and state name are the same, its a state
-        if(data[i].county_name == data[i].state_name){
+        //if county and state name are not the same, its just a county
+        if(data[i].county_name != data[i].state_name){
             //first push up to the number specified
-            console.log(data[i])
             if(greatestDeaths.length < number){
                 greatestDeaths.push(data[i])
             }
@@ -21,10 +24,8 @@ const findGreatest = (data, number) => {
                         greatestDeaths[j] = data[i]
                         //if we make a replacement we need to take out the smallest and not the current
                         //value from the list
-                        console.log(current)
                         for(var k = 0; k < number; ++k){
                             if(current != null && greatestDeaths[k].death < current.death){
-                                console.log(greatestDeaths[k])
                                 let temp = greatestDeaths[k]
                                 greatestDeaths[k] = current
                                 current = temp
@@ -35,16 +36,72 @@ const findGreatest = (data, number) => {
                     }
                     
                 }
+                
             }
-        }
+            //now do that for all the other arrays
+            //first push up to the number specified
+            if(greatestCases.length < number){
+                greatestCases.push(data[i])
+            }
+            else{
+                //then start replacing when a greater is found
+                for(var j = 0; j < number; ++j){
+                    if(data[i].confirmed > greatestCases[j].confirmed){
+                        current = greatestCases[j]
+                        greatestCases[j] = data[i]
+                        //if we make a replacement we need to take out the smallest and not the current
+                        //value from the list
+                        for(var k = 0; k < number; ++k){
+                            if(current != null && greatestCases[k].confirmed < current.confirmed){
+                                let temp = greatestCases[k]
+                                greatestCases[k] = current
+                                current = temp
+                                break
+                            }
+                        }
+                        break
+                    }
+                    
+                }
+                
+            }
+
+            if(data[i].fatality_rate != "nan%"  && data[i].fatality_rate != "inf%" && greatestFatality.length < number){
+                greatestFatality.push(data[i])
+            }
+            else{
+                //then start replacing when a greater is found
+                for(var j = 0; j < number; ++j){
+                    if(data[i].fatality_rate != "nan%" && data[i].fatality_rate != "inf%" && data[i].fatality_rate > greatestFatality[j].fatality_rate){
+                        current = greatestFatality[j]
+                        greatestFatality[j] = data[i]
+                        //if we make a replacement we need to take out the smallest and not the current
+                        //value from the list
+                        for(var k = 0; k < number; ++k){
+                            if(current != null && greatestFatality[k].fatality_rate < current.fatality_rate){
+                                let temp = greatestFatality[k]
+                                greatestFatality[k] = current
+                                current = temp
+                                break
+                            }
+                        }
+                        break
+                    }
+                    
+                }
+                
+            }
+        }    
     }
-    console.log(greatestDeaths)
+    let allGreatest = new Array(greatestDeaths, greatestCases, greatestFatality)
+    return allGreatest
 }
 
 
 const topCounties = (props) => {
     let data = props.data
-    findGreatest(data, 5)
+    let greatest = findGreatest(data, 5)
+    console.log(greatest)
     return (
         <div class="side" id="topContainer">
             <div id="topSelector">
