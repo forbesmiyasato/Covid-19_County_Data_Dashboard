0e6c6bd96228f2604cc006c3a8299b4affc284be
ThomasMLawrence <tmlawrence54@gmail.com>
2020-08-05 09:46:26 -0700

    test fatality

--- client/src/components/topCounties.js
+++ client/src/components/topCounties.js
@@ -11,7 +11,7 @@ const findGreatest = (data, number) => {
     let current = null;
     for(var i = 0; i < data.length; ++i){
         //if county and state name are not the same, its just a county
-        if(data[i].county_name != data[i].state_name){
+        
             //first push up to the number specified
             if(greatestDeaths.length < number){
                 greatestDeaths.push(data[i])
@@ -72,7 +72,7 @@ const findGreatest = (data, number) => {
             else{
                 //then start replacing when a greater is found
                 for(var j = 0; j < number; ++j){
-                    if(data[i].fatality_rate != "nan%" && data[i].fatality_rate != "inf%" && data[i].fatality_rate > greatestFatality[j].fatality_rate){
+                    if(data[i].fatality_rate > greatestFatality[j].fatality_rate){
                         current = greatestFatality[j]
                         greatestFatality[j] = data[i]
                         //if we make a replacement we need to take out the smallest and not the current
@@ -91,7 +91,7 @@ const findGreatest = (data, number) => {
                 }
                 
             }
-        }    
+            
     }
     let allGreatest = new Array(greatestDeaths, greatestCases, greatestFatality)
     return allGreatest
