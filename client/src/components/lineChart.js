4326e9485e4ef422aa9dcfff402b646c52a5c5b2
forbesmiyasato <miya5288@pacificu.edu>
2020-08-08 17:19:39 -0700

    added null check to avoid crashing, need to find reason though

--- client/src/components/lineChart.js
+++ client/src/components/lineChart.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 
 const LineChart = (props) => {
@@ -7,35 +7,36 @@ const LineChart = (props) => {
     const [labels, setLabels] = useState([]);
     const [data, setData] = useState([]);
 
-    useEffect (() => {
+    useEffect(() => {
         const labelsArray = [];
         const dataArray = [];
         let prev = 0;
-        for (const[key, value] of Object.entries(timelineData)) {
-            labelsArray.push(key);
-            dataArray.push(value[type] - prev);
-            prev = value[type];
-        }
+        if (timelineData) {
+            for (const [key, value] of Object.entries(timelineData)) {
+                labelsArray.push(key);
+                dataArray.push(value[type] - prev);
+                prev = value[type];
+            }
 
-        setLabels(labelsArray);
-        setData(dataArray);
-    }, [])
+            setLabels(labelsArray);
+            setData(dataArray);
+        }
+    }, []);
 
     console.log(timelineData);
     console.log(labels);
     console.log(data);
 
-
     const chartData = {
         labels: labels,
         datasets: [
             {
-                label: type === 'cases' ? 'New Cases' : 'New Deaths',
+                label: type === "cases" ? "New Cases" : "New Deaths",
                 data: data,
                 fill: true,
                 backgroundColor: "rgba(75,192,192,0.2)",
                 borderColor: "rgba(75,192,192,1)",
-            }
+            },
         ],
     };
 
