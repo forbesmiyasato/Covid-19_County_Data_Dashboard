42b992df73afa043bd8df5a4adc349ae60b4512d
forbesmiyasato <miya5288@pacificu.edu>
2020-08-09 19:42:20 -0700

    Done. Line chart color matches theme color

--- client/src/components/lineChart.js
+++ client/src/components/lineChart.js
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
+import { getColor1, getColor3 } from "./colors";
 
 const LineChart = (props) => {
     const timelineData = props.data;
     const type = props.type;
     const [labels, setLabels] = useState([]);
     const [data, setData] = useState([]);
+
+    //converter from https://stackoverflow.com/questions/21646738/convert-hex-to-rgba
+    function hexToRgbA(hex){
+        var c;
+        if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+            c= hex.substring(1).split('');
+            if(c.length== 3){
+                c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+            }
+            c= '0x'+c.join('');
+            return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',0.3)';
+        }
+        throw new Error('Bad Hex');
+    }
 
     useEffect(() => {
         const labelsArray = [];
@@ -34,8 +49,8 @@ const LineChart = (props) => {
                 label: type === "cases" ? "New Cases" : "New Deaths",
                 data: data,
                 fill: true,
-                backgroundColor: "rgba(75,192,192,0.2)",
-                borderColor: "rgba(75,192,192,1)",
+                backgroundColor: hexToRgbA(getColor1()),
+                borderColor: getColor1(),
             },
         ],
     };
